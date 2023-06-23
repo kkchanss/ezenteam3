bec898e0a1a0ddfcfb1549c63d38d91bba825f3b
504 <504@DESKTOP-OM2NF7O>
2023-06-23 17:57:12 +0900

    한강라면코드연습(신예지)

---
+++ team3_project/public/브랜치/한강라면_코드연습(신예지).js
@@ -0,0 +1,32 @@
+let 그릇 = []
+let 물온도 = []
+let 인덕션온도 = []
+let 타이머 = []
+
+function 전원(){
+	// 재료를 입력 받기.
+	for ( let i = 0 ; ture ; i++){
+		let 재료 = prompt('넣고 싶은 재료를 입력해주세요.')
+		 if (재료 == ` `){
+			 alert('재료를 다시 입력해주세요.')
+		 } 
+		 else {
+			그릇.push(재료); break;
+		 }
+	}
+
+
+	// 라면조리 시작하기 (실행)
+	for ( let i = 0 ; ture ; i++ ) {
+		let 실행 = 1;
+		if( 재료.length >= 1){
+			
+		}
+		
+	}
+	
+	
+	
+	
+	
+}
