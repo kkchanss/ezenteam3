0171d2d652ea90a833ad014ef9e92b9999c2631e
504 <504@DESKTOP-OM2NF7O>
2023-06-26 11:42:33 +0900

    한강라면 코드연습 신예지

--- team3_project/public/브랜치/한강라면_코드연습(신예지).js
+++ team3_project/public/브랜치/한강라면_코드연습(신예지).js
@@ -17,16 +17,32 @@ function 전원(){
 
 
 	// 라면조리 시작하기 (실행)
-	for ( let i = 0 ; ture ; i++ ) {
-		let 실행 = 1;
+	for ( let i = 0 ; true ; i++ ) {
+		let 실행 = prompt( '1. 실행' )
 		if( 재료.length >= 1){
-			
+			if(  실행 == 1) break;
 		}
-		
+	}	
+	
+	//물 온도가 95도가 될때까지
+	for ( let i = 0; true ; 물온도++){
+		if (물온도 >= 95) break;
 	}
 	
+	//가열된 물 그릇에 넣기
+	그릇.push(물_온도)
+	
+	
+	// 인덕션온도가 8단계가 될때까지
+	for ( let i = 0; true ; 인덕션온도++){
+		if (인덕션온도 != 8) break;
+	}
+	
+	// 180초가 지날때까지 반복하기
 	
 	
 	
+	
+	console.log('종료합니다.')
 	
 }
