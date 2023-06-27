c608cf50b85bf12af6e6ecd456d2a9f034fab702
504 <504@DESKTOP-T7B8DGK>
2023-06-27 15:44:28 +0900

    0627_틱택토_클릭함수commit

--- team3_project/public/과제/과제2/틱택토.js
+++ team3_project/public/과제/과제2/틱택토.js
@@ -1,10 +1,10 @@
-/**
- * 
+/*
+
  */
 
  
 // 1. 게임판함수
-
+let gameBox = [0,0,0,0,0,0,0,0,0];
 
 
 
@@ -16,8 +16,19 @@
 
 // 2. 클릭함수
 
+function 클릭함수(index) {
+	//let btn_check = document.querySelector('.btn${i}');
+	if(gameBox[index] != 0) {
+		alert('이미 선택된 칸입니다. 다시 선택해주세요.')
+		return;
+	}
+	
+	gameBox[index] == 1;
+	document.querySelector('.btn${i}').innerHTML = "O";
+	
+}
 
-
+document.querySelector('.btn0').innerHTML = "O";
 
 
 
