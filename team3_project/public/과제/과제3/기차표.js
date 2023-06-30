28efaab0222df29a50059ea9427b037996044809
504 <504@DESKTOP-T7B8DGK>
2023-06-30 15:54:42 +0900

    0630_좌석()

--- team3_project/public/과제/과제3/기차표.js
+++ team3_project/public/과제/과제3/기차표.js
@@ -18,9 +18,42 @@
 
 
 
+
+// 좌석선택 -- 김찬희
+
+let 티켓리스트 = [];
+let 티켓 = { 좌석 : 1 };
+
+function 좌석선택(index) {
+	
+}
+
+
+
 // 좌석 -- 김찬희
 
 
+
+function 좌석(){
+	
+	let seat = document.querySelector('.seat');
+	let lists = "";
+	
+	
+	for(let i = 1; i<=28; i++) {
+		lists+=`<button class="seat${i} onclick="좌석선택(${i})">${i}</button>`;
+		if(i % 7 == 0) lists+="<br/>";
+		if(i % 14 == 0) lists+="<br/>";
+	}
+	
+	seat.innerHTML = lists;
+
+	for(let i = 0; i < 티켓리스트.length; i++) {
+		let seats = 티켓리스트[i].티켓; 
+		document.querySelector(`.seat${seats}`).style=`color: #406596; background-color: white;`;
+	}	
+	
+}
 
 
 
