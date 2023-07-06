340292f1d777a465a9bef9ab2b6b1115e8c0c5ef
504 <504@DESKTOP-T7B8DGK>
2023-07-06 17:58:37 +0900

    0706_headerPageCommet

--- subway/src/main/webapp/js/mainPage.js
+++ subway/src/main/webapp/js/mainPage.js
@@ -1,2 +1,5 @@
 // 광고 이미지 배열
 let simgList = ['main_h_img01.jpg', 'main_h_img01.jpg', 'main_h_img01.jpg']; 
+
+// 메인메뉴 리스트
+let mainMenuList = ['eggmayo.png','ham.png','shrimp.png','BBQ.png','chicken_teriyaki.png'];
