de64c47c024d7e458f38a5f19af1de3416760001
504 <504@DESKTOP-OM2NF7O>
2023-07-12 15:39:29 +0900

    제품페이지 CSS수정

--- subway/src/main/webapp/js/orderPage.js
+++ subway/src/main/webapp/js/orderPage.js
@@ -8,7 +8,7 @@
  console.log ('orderPage JS 실행')
  
  // ★☆★☆★☆★☆  제품 페이지에서 선택된 아이템 임시 설정. 추후에 수정해야함 ★☆★☆★☆★☆ 
- let selectIime = JSON.parse(localStorage.getItem('selectIime', selectIime));
+ let selectItem = JSON.parse(localStorage.getItem('selectItem'));
  
  
 
